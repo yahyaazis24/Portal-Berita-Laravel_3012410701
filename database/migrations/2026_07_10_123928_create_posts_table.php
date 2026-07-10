4837394a475181473c30e9389f58e0a5e0314a0a
yahyaazis24 <yahya.azis24@student.uisi.ac.id>
2026-07-11 03:18:34 +0700

    add table content and image in migration

--- database/migrations/2026_07_10_123928_create_posts_table.php
+++ database/migrations/2026_07_10_123928_create_posts_table.php
@@ -14,6 +14,8 @@ return new class extends Migration
     Schema::create('posts', function (Blueprint $table) {
         $table->id();
         $table->string('title');
+        $table->text('content');
+        $table->string('image');
         $table->string('published');
         $table->timestamps();
     });
